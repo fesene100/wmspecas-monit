9128e70a84412c5e53062db68e69dda72e33939f
fesene100 <felipe@inovaetech.com.br>
2024-09-23 08:43:31 -0300

    v1.2.3

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Title, IconButton, useTheme } from "@inovaetech/components-react";
+import { Title, useTheme, ButtonTheme } from "@inovaetech/components-react";
 
 export const Header = ({ title, children }: { title: string; children?: React.ReactNode }) => {
   const { isDark, changeTheme } = useTheme();
@@ -6,15 +6,11 @@ export const Header = ({ title, children }: { title: string; children?: React.Re
   return (
     <Title title={title}>
       {children}
-      <IconButton
+      <ButtonTheme
         onPress={() => {
           changeTheme(isDark ? "light" : "dark");
         }}
-        isRaised
-        circle
-        color="warning"
-        className="h-8 w-8"
-        icon={isDark ? "PiMoonFill" : "PiSunFill"}
+        theme={isDark ? "dark" : "light"}
       />
     </Title>
   );
